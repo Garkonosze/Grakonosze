cef4e3653103b9aac26867d6e23b34fc456af0c9
Olciaaa <57765820+Olciaaa@users.noreply.github.com>
2024-05-18 11:58:59 +0200

    buttons and navigation (#8)
    Co-authored-by: aleksandraposkrobek <aleksandrap@remitly.com>

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -3,18 +3,20 @@ import NetworkView from "./src/screens/NetworkView";
 import XorPlaneView from "./src/screens/XorPlaneView";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainScreen from "./src/screens/MainScreen";
 
 export type RootStackParamList = {
   NetworkView: undefined;
   XorPlaneView: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="NetworkView">
+      <Stack.Navigator initialRouteName="MainScreen">
+        <Stack.Screen name="MainScreen" component={MainScreen} />
         <Stack.Screen name="NetworkView" component={NetworkView} />
         <Stack.Screen name="XorPlaneView" component={XorPlaneView} />
       </Stack.Navigator>
