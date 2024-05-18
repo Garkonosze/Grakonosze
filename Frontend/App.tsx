aa2fb6a42dce3f095317756d61bedef1f0861761
Rusiek <szymon.rusiecki@proton.me>
2024-05-18 10:20:34 +0200

    Neural Network makes brrr in tsx! v3.1

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,18 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NetworkView from './src/screens/NetworkView';
+import XorPlaneView from './src/screens/XorPlaneView';
 
-export default function App() {
+export type RootStackParamList = {
+  NetworkView: undefined;
+  XorPlaneView: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sialala</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NetworkView">
+        <Stack.Screen name="NetworkView" component={NetworkView} />
+        <Stack.Screen name="XorPlaneView" component={XorPlaneView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
